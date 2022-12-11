e1bbf6c79ba55de6821af40df4cd05f023dcdf2f
mac <maciekklospl@gmail.com>
2022-12-11 07:05:07 +0000

    create signup and login user services

--- server/services/user.ts
+++ server/services/user.ts
@@ -1,5 +1,11 @@
+import { AuthenticationError, ForbiddenError } from 'apollo-server-micro';
+import { setCookies } from 'cookies-next';
 import { OptionsType } from 'cookies-next/lib/types';
-import { User } from 'server/models/user';
+import errorHandler from 'server/controllers/error.controller';
+import UserModel, { User } from 'server/models/user';
+import { LoginInput } from 'server/schemas/user';
+import { Context } from 'server/types/context';
+import { disconnectDB } from 'server/utils/connectDB';
 import redisClient from 'server/utils/connectRedis';
 import { signJwt } from 'server/utils/jwt';
 
@@ -43,3 +49,76 @@ const signTokens = (user: User) => {
 
     return { accessToken, refreshToken };
 };
+
+const findByEmail = async (email: string): Promise<User | null> =>
+    UserModel.findOne({ email }).select('+password');
+
+export default class UserService {
+    signUpUser = async (input: Partial<User>) => {
+        try {
+            const user = await UserModel.create(input);
+            await disconnectDB();
+            return {
+                status: 'success',
+                user: user.toJSON(),
+            };
+        } catch (error: any) {
+            if (error.code === 11000) {
+                return new ForbiddenError('Email already exists');
+            }
+            errorHandler(error);
+            return null;
+        }
+    };
+
+    loginUser = async (input: LoginInput, { req, res }: Context) => {
+        try {
+            const message = 'Invalid email or password';
+            // Find user by email
+            const user = await findByEmail(input.email);
+            await disconnectDB();
+
+            if (!user) {
+                return new AuthenticationError(message);
+            }
+
+            // Compare passwords
+            if (
+                !(await UserModel.comparePasswords(
+                    user.password,
+                    input.password
+                ))
+            ) {
+                return new AuthenticationError(message);
+            }
+
+            // Sign JWT Tokens
+            const { accessToken, refreshToken } = signTokens(user);
+
+            // Add Tokens to Context
+            setCookies('access_token', accessToken, {
+                req,
+                res,
+                ...accessTokenCookieOptions,
+            });
+            setCookies('refresh_token', refreshToken, {
+                req,
+                res,
+                ...refreshTokenCookieOptions,
+            });
+            setCookies('logged_in', 'true', {
+                req,
+                res,
+                ...accessTokenCookieOptions,
+                httpOnly: false,
+            });
+            return {
+                status: 'success',
+                accessToken,
+            };
+        } catch (error: any) {
+            errorHandler(error);
+            return null;
+        }
+    };
+}
