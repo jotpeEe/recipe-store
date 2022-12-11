7b0dfd7c89140c0537cb7c5b4a11b85c760ae0ee
mac <maciekklospl@gmail.com>
2022-12-11 04:15:42 +0000

    create user schema

---
+++ server/schemas/user/index.ts
@@ -0,0 +1,5 @@
+export { default as LoginInput } from './loginInput';
+export { default as LoginResonse } from './loginResponse';
+export { default as UserResponse } from './response';
+export { default as SignUpInput } from './signUpInput';
+export { default as UserData } from './data';
---
+++ server/schemas/user/response.ts
@@ -0,0 +1,13 @@
+import { Field, ObjectType } from 'type-graphql';
+import UserData from './data';
+
+@ObjectType()
+class UserResponse {
+    @Field(() => String)
+    status: string;
+
+    @Field(() => UserData)
+    user: UserData;
+}
+
+export default UserResponse;
