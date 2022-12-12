98c76c74df3a05222b62e49a83a4b091cc130ef8
mac <maciekklospl@gmail.com>
2022-12-12 21:00:45 +0000

    added poppins and roboto fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,9 @@
 import '@styles/globals.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import type { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => (
