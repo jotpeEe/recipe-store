5263c616f1f7aaf9969188814ae291ee1b12dcb3
mac <maciekklospl@gmail.com>
2022-12-12 21:02:29 +0000

    global font size setup, customized theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-        './app/**/*.{js,ts,jsx,tsx}',
-    ],
+    content: ['./pages/**/*.{js,ts,jsx,tsx}', './client/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            gridTemplateColumns: {
+                '1to2': '1fr 2fr',
+            },
+            colors: {
+                primary: '#129575',
+                secondary: '#71B1A1',
+            },
+            spacing: {
+                section: '35rem',
+            },
+            boxShadow: {
+                card: '0px 2px 8px 0px rgba(95, 126, 155, 0.3)',
+            },
+        },
+        fontFamily: {
+            sans: ['Poppins', 'system-ui', 'sans-serif'],
+            sans2: ['Roboto', 'system-ui', 'sans-serif'],
+        },
     },
     plugins: [],
 };
