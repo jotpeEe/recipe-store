ee6ab2616fa3b126caad1f3acc5358afc842fe39
mac <maciekklospl@gmail.com>
2022-12-12 21:03:26 +0000

    create import aliases for frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+
+import { PageLayout as Layout } from '@layouts';
+import { Recipes, Reviews } from '@sections';
 
 /**
  * Home: The Landing page of the web app
