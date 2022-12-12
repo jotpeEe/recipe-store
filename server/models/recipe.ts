c774db3b18882f4e417d23b938c4f2e4a322e4c0
mac <maciekklospl@gmail.com>
2022-12-12 12:50:50 +0000

    create recipe model

---
+++ server/models/recipe.ts
@@ -0,0 +1,45 @@
+import {
+    getModelForClass,
+    ModelOptions,
+    prop,
+    Severity,
+} from '@typegoose/typegoose';
+import type { Ref } from '@typegoose/typegoose';
+
+import { User } from '@models/user';
+
+@ModelOptions({
+    schemaOptions: {
+        timestamps: true,
+    },
+    options: {
+        allowMixed: Severity.ALLOW,
+    },
+})
+export class Recipe {
+    readonly _id: string;
+
+    @prop({ required: true })
+    title: string;
+
+    @prop({ required: true })
+    description: string;
+
+    @prop({ required: true })
+    prep_time: number;
+
+    @prop({ required: true })
+    cuisine: string;
+
+    @prop({ required: true })
+    servings: number;
+
+    @prop({ required: true })
+    image: string;
+
+    @prop({ required: true, ref: () => User })
+    user: Ref<User>;
+}
+
+const RecipeModel = getModelForClass<typeof Recipe>(Recipe);
+export default RecipeModel;
