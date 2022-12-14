a20382ef59d9b92de6024e287b5c7b5ee3521c20
mac <maciekklospl@gmail.com>
2022-12-14 06:24:51 +0000

    updated RecipeCard

--- client/components/RecipeCard.tsx
+++ client/components/RecipeCard.tsx
@@ -1,8 +1,11 @@
 import Image from 'next/image';
 
-const Card: React.FC = () => (
-    <div className=" relative flex flex-col justify-center items-center h-230 rounded-lg ">
+import { Card, Title, Link } from './CardWithLinks';
+
+const RecipeCard: React.FC = () => (
+    <Card className="flex flex-col justify-center items-center h-230 rounded-lg ">
         <div className="absolute bottom-0 left-0 bg-gray-100 w-full h-3/4 rounded-lg"></div>
+
         <div className="relative">
             <Image
                 width={110}
@@ -11,6 +14,7 @@ const Card: React.FC = () => (
                 alt="recipe image"
                 className="z-10 rounded-full "
             />
+
             <div className="absolute flex items-center justify-center text-xs leading-normal py-1 px-2 rounded-3xl bg-yellow-100 -right-6 top-1/2 -translate-y-3/4 ">
                 <svg
                     width="10"
@@ -28,15 +32,22 @@ const Card: React.FC = () => (
             </div>
         </div>
 
-        <div className="z-10 font-semibold py-4 w-9/12 flex justify-center items-center">
-            Classic Greek Salad
-        </div>
+        <h5 className="z-10 font-sans2 py-4 w-9/12 flex justify-center items-center ">
+            <Title className="font-sans" href="https://mklos.co">
+                Classic Greek Salad
+            </Title>
+        </h5>
+
         <div className="z-10 flex justify-between items-center w-full">
             <div className="flex flex-col items-start p-3">
                 <div className="text-xs pb-1">Time</div>
                 <div className="text-xs font-semibold">15 mins</div>
             </div>
-            <button className="flex justify-center items-center p-1 h-fit rounded-full bg-white mx-3">
+
+            <Link
+                href="https://v1.mklos.co"
+                className="flex justify-center items-center p-1 h-fit rounded-full bg-white mx-3"
+            >
                 <svg
                     width="16"
                     height="16"
@@ -49,9 +60,9 @@ const Card: React.FC = () => (
                         fill="#71B1A1"
                     />
                 </svg>
-            </button>
+            </Link>
         </div>
-    </div>
+    </Card>
 );
 
-export default Card;
+export default RecipeCard;
