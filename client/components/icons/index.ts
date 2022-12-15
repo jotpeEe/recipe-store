3a084936b10bb28863422e14002ee436a82aa2ee
mac <maciekklospl@gmail.com>
2022-12-15 07:24:18 +0000

    create svg Icons component

---
+++ client/components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as IconArrow } from './Arrow';
+export { default as IconFacebook } from './Facebook';
+export { default as IconGoogle } from './Google';
+export { default as IconWarning } from './Warning';
