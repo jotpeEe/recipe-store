9621adbc9eb7c48c75823835a2bf4cef074ed687
mac <maciekklospl@gmail.com>
2022-12-17 10:21:03 +0000

    create ImageUpload component

--- constants.ts
+++ constants.ts
@@ -1,12 +1,23 @@
 export const GRAPHQL_ENDPOINT = process.env
     .NEXT_PUBLIC_GRAPHQL_ENDPOINT as string;
+
 export const LOCAL_URI = process.env.MONGODB_LOCAL_URI as string;
+
 export const redisURL = 'redis://localhost:6379';
+
 export const accessTokenPrivateKey = process.env
     .ACCESS_TOKEN_PRIVATE_KEY as string;
+
 export const refreshTokenPrivateKey = process.env
     .REFRESH_TOKEN_PRIVATE_KEY as string;
+
 export const accessTokenPublicKey = process.env
     .ACCESS_TOKEN_PUBLIC_KEY as string;
+
 export const refreshTokenPublicKey = process.env
     .REFRESH_TOKEN_PUBLIC_KEY as string;
+
+const CLAUDINARY_ACCOUNT_NAME = process.env
+    .NEXT_PUBLIC_CLOUDINARY_ACCOUNT_NAME as string;
+
+export const CLDN_API_URL = `https://api.cloudinary.com/v1_1/${CLAUDINARY_ACCOUNT_NAME}/image/upload`;
