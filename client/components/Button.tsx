12fa7eaf0e7e9ee4e443f59d242f794b58c1c9a7
mac <maciekklospl@gmail.com>
2022-12-19 10:23:28 +0000

    update Button component
    before: line height interrupted with search bar
    in profile page, providint unnecessary margins.
    
    after: adjust line height to the content, if needed
    adjusting with margin or padding when combined with
    svg icons.

--- client/components/Button.tsx
+++ client/components/Button.tsx
@@ -29,8 +29,8 @@ const Button: FC<ButtonProps> = ({
     <button
         type={type}
         className={classNames(
-            'flex items-center justify-center bg-primary gap-2 text-white font-semibold rounded-xl hover:bg-green-600 transition-all',
-            size === 'small' && 'text-xs py-2 px-5 ',
+            'flex items-center justify-center bg-primary gap-2 text-white font-semibold rounded-xl hover:bg-green-600 transition-all h-fit',
+            size === 'small' && 'text-xs py-2 px-5 leading-none',
             size === 'small' && outlined && 'font-normal',
             size === 'medium' && 'px-5 py-2',
             size === 'large' && 'px-12 py-4',
