b0c8705435e6aadcafc6adad61672caeaf2f3a4a
mac <maciekklospl@gmail.com>
2023-01-29 20:23:46 +0100

    Reviews section update
    b: mockup for testing layout.
    
    a: fully working component with implemented
    queries and slider feature as a display method.

--- client/sections/Reviews.tsx
+++ client/sections/Reviews.tsx
@@ -1,26 +1,35 @@
-import { SectionTitle, ReviewCard, CardList } from '@components';
+import { Slider, SectionTitle, ReviewCard } from '@components';
+import { useGetLastReviewsQuery } from '@generated/graphql';
 import { SectionLayout as Layout } from '@layouts';
+import { requestClient } from '@requests';
 
-const Reviews: React.FC = () => (
-    <Layout id="reviews">
-        <div className="grid grid-col-1 self-start">
-            <SectionTitle
-                subtitle="Reviews"
-                title="Share your experience with others"
-                description="If you tried a recipe, let creator know how you feel about it."
-            />
-            <CardList>
-                <ReviewCard />
-                <ReviewCard />
-                <ReviewCard />
-            </CardList>
-            <CardList>
-                <ReviewCard />
-                <ReviewCard />
-                <ReviewCard />
-            </CardList>
-        </div>
-    </Layout>
-);
+const Reviews: React.FC = () => {
+    const { data: reviews } = useGetLastReviewsQuery(
+        requestClient,
+        {},
+        {
+            select: res => res.getLastReviews.reviews,
+        }
+    );
+
+    if (!reviews) return null;
+
+    return (
+        <Layout id="reviews">
+            <div className="grid grid-col-1 self-start">
+                <SectionTitle
+                    subtitle="Reviews"
+                    title="Share your experience with others"
+                    description="If you tried a recipe, let creator know how you feel about it."
+                />
+                <Slider controller>
+                    {reviews.map((review, index) => (
+                        <ReviewCard key={index} {...review} />
+                    ))}
+                </Slider>
+            </div>
+        </Layout>
+    );
+};
 
 export default Reviews;
