4b77ac129c29df29802fe113e0a6d12b9a26afee
mac <maciekklospl@gmail.com>
2023-01-29 19:56:39 +0100

    tsconfig and tailwindconfig updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const plugin = require('tailwindcss/plugin');
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './client/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
             gridTemplateColumns: {
                 '1to2': '1fr 2fr',
+                slider: 'repeat(12, minmax(0, 1fr)',
+                fill: 'repeat(auto-fill, minmax(125px, 1fr))',
             },
             colors: {
                 primary: '#129575',
@@ -18,11 +24,26 @@ module.exports = {
             spacing: {
                 section: '35rem',
                 'header-p': '100px',
+                space0: '0%',
+                space1: '100%',
+                space2: '200%',
+                space3: '300%',
+                space4: '400%',
+                space5: '500%',
+                space6: '600%',
+                space7: '700%',
+                space8: '800%',
+                space9: '900%',
+                space10: '1000%',
+                space11: '1100%',
             },
             boxShadow: {
                 card: '0px 2px 8px 0px rgba(95, 126, 155, 0.3)',
                 form: '0px 10px 34px 0px rgb(95 126 155 / 18%)',
                 border: '0px 0px 0px 1px inset',
+            },
+            minHeight: {
+                textarea: '6rem',
             },
             maxWidth: {
                 '3/4': '75%',
@@ -33,5 +54,9 @@ module.exports = {
             sans2: ['Roboto', 'system-ui', 'sans-serif'],
         },
     },
-    plugins: [],
+    plugins: [
+        plugin(({ addVariant }) => {
+            addVariant('children', '&>*');
+        }),
+    ],
 };
