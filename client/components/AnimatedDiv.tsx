d2af42fecc23f29323ea69687ef76cf076c8eaa5
mac <maciekklospl@gmail.com>
2023-01-29 17:39:40 +0100

    Animated component implementation for Recipe component

---
+++ client/components/AnimatedDiv.tsx
@@ -0,0 +1,30 @@
+import { FC, ReactNode, useEffect, useState } from 'react';
+
+import classNames from 'classnames';
+
+type AnimatedProps = {
+    children: ReactNode;
+    className?: string;
+};
+
+const Animated: FC<AnimatedProps> = ({ children, className }) => {
+    const [active, setActive] = useState(false);
+
+    useEffect(() => {
+        setActive(true);
+    }, []);
+
+    return (
+        <div
+            className={classNames(
+                'transition ease-in-out duration-300 origin-top',
+                active ? 'opacity-1' : 'opacity-0',
+                className
+            )}
+        >
+            {children}
+        </div>
+    );
+};
+
+export default Animated;
