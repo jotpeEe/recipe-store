1638b3df92000c4a9b744c4f526f5a7cad7239d7
mac <maciekklospl@gmail.com>
2023-01-29 19:44:42 +0100

    RecipeList component update
    b: was just for the sake of displaying
    an example.
    
    a: implemendted queries for data, used for profile
    and hero page recipe listing with switch working as
    a filter.

--- client/components/RecipesList.tsx
+++ client/components/RecipesList.tsx
@@ -1,25 +1,92 @@
-import { FC } from 'react';
+import { FC, useCallback, useMemo, useState } from 'react';
 
+import { RECIPE_LIMIT } from '@constants';
 import { IRecipe } from '@lib/types';
 
-import Categories from './Categories';
-import RecipeCard from './RecipeCard';
+import Button from './Button';
+import RecipeCard from './card/Recipe';
+import Switch from './Switch';
 
 type RecipesListProps = {
     className?: string;
     recipes?: Partial<IRecipe>[];
 };
 
-const RecipesList: FC<RecipesListProps> = ({ className, recipes }) => (
-    <div className={`${className}`}>
-        <Categories />
-        <div className={`grid grid-cols-4 gap-12`}>
-            {recipes &&
-                recipes.map((recipe, index) => (
-                    <RecipeCard key={index} recipe={recipe} />
-                ))}
+const RecipesList: FC<RecipesListProps> = ({ className, recipes }) => {
+    const [active, setActive] = useState(0);
+    const [limit, setLimit] = useState(RECIPE_LIMIT);
+
+    const cuisine = useMemo(
+        () =>
+            recipes?.reduce(
+                (acc, cv) => {
+                    if (cv.cuisine) {
+                        if (acc.find(element => element === cv.cuisine))
+                            return acc;
+                        acc.push(cv.cuisine);
+                        return acc;
+                    }
+
+                    return acc;
+                },
+                ['All'] as string[]
+            ),
+        [recipes]
+    );
+
+    const recipeList = useMemo(() => {
+        if (cuisine && recipes) {
+            const filter = cuisine[active];
+
+            if (filter === 'All') return recipes.slice(0, limit);
+
+            const list = recipes
+                .filter(recipe => recipe.cuisine === filter)
+                .slice(0, limit);
+
+            return list;
+        }
+
+        return undefined;
+    }, [recipes, limit, active]);
+
+    const handleClick = useCallback(() => {
+        setLimit(prevLimit => prevLimit + 4);
+    }, []);
+
+    return (
+        <div className={`${className} children:mb-4`}>
+            <div className="flex gap-3 w-fit">
+                <Switch
+                    array={cuisine}
+                    active={active}
+                    setActive={setActive}
+                    size="sm"
+                />
+            </div>
+            <div
+                className={`grid grid-cols-fill gap-12 overflow-y-auto overflow-x-hidden`}
+            >
+                {recipeList &&
+                    recipeList.map((recipe, index) => (
+                        <RecipeCard key={index} recipe={recipe} />
+                    ))}
+                <div className="self-center">
+                    <Button href="/create" className="mb-3" size="sm">
+                        Create new recipe
+                    </Button>
+                    <Button
+                        onClick={handleClick}
+                        className="self-center"
+                        outlined
+                        size="sm"
+                    >
+                        View more recipes
+                    </Button>
+                </div>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default RecipesList;
