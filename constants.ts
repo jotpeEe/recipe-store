edbda80f5b1b323d3f51817dc2b83a2467d2f5a5
mac <maciekklospl@gmail.com>
2023-01-29 16:49:10 +0100

    extracted project constants into one file

--- constants.ts
+++ constants.ts
@@ -1,9 +1,17 @@
-export const GRAPHQL_ENDPOINT = process.env
-    .NEXT_PUBLIC_GRAPHQL_ENDPOINT as string;
+export const GRAPHQL_ENDPOINT =
+    process.env.NODE_ENV === 'production'
+        ? 'https://recipes.mklos.co/api/graphql'
+        : 'http://localhost:3000/api/graphql';
 
-export const LOCAL_URI = process.env.MONGODB_LOCAL_URI as string;
+export const MONGODB_URI =
+    process.env.NODE_ENV === 'production'
+        ? (process.env.MONGODB_LOCAL_URI as string)
+        : (process.env.MONGODB_LOCAL_URI_DEVELOP as string);
 
-export const redisURL = 'redis://localhost:6379';
+export const REDIS_URL =
+    process.env.NODE_ENV === 'production'
+        ? (process.env.REDIS_LOCAL_URL as string)
+        : 'redis://localhost:6379';
 
 export const accessTokenPrivateKey = process.env
     .ACCESS_TOKEN_PRIVATE_KEY as string;
@@ -17,7 +25,14 @@ export const accessTokenPublicKey = process.env
 export const refreshTokenPublicKey = process.env
     .REFRESH_TOKEN_PUBLIC_KEY as string;
 
-const CLAUDINARY_ACCOUNT_NAME = process.env
-    .NEXT_PUBLIC_CLOUDINARY_ACCOUNT_NAME as string;
+export const RECIPE_LIMIT = 7;
 
-export const CLDN_API_URL = `https://api.cloudinary.com/v1_1/${CLAUDINARY_ACCOUNT_NAME}/image/upload`;
+export const cookies = {
+    ACCESS_TOKEN: 'access_token',
+    REFRESH_TOKEN: 'refresh_token',
+    RECIPE_INFO: 'recipe_info',
+    LOGGED_IN: 'logged_in',
+};
+
+export const CLDN_API_URL =
+    'https://api.cloudinary.com/v1_1/dxkgc7cab/image/upload';
