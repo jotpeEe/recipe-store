f1479bdbc15f83da95c852629aa91390c13b4333
mac <maciekklospl@gmail.com>
2023-01-29 17:58:23 +0100

    SectionTitle update

--- client/components/SectionTitle.tsx
+++ client/components/SectionTitle.tsx
@@ -1,25 +1,44 @@
+import classNames from 'classnames';
+
 import Button from './Button';
 
 type SectionTitleProps = {
     subtitle?: string;
     title: string;
     description: string;
-    button?: string;
-    arrow?: boolean;
+    center?: boolean;
+    href?: string;
+    buttonText?: string;
 };
 
 const SectionTitle: React.FC<SectionTitleProps> = ({
     subtitle,
     title,
     description,
-    button,
-    arrow = true,
+    href,
+    center = false,
+    buttonText,
 }) => (
-    <div className="flex flex-col justify-start text-left align-start max-w-lg">
+    <div
+        className={classNames(
+            'flex flex-col lg:max-w-[40ch] md:max-w-xl md:text-left',
+            center
+                ? 'text-center justify-center align-center'
+                : 'text-left justify-start align-start max-w-xl'
+        )}
+    >
         <h5 className="pb-4">{subtitle}</h5>
         <h2 className="pb-6">{title}</h2>
         <h4 className="pb-6">{description}</h4>
-        {button && <Button arrow={arrow}>{button}</Button>}
+        {href && (
+            <Button
+                className={`${center && 'md:mx-0 mx-auto'}`}
+                href={href}
+                arrow
+            >
+                {buttonText}
+            </Button>
+        )}
     </div>
 );
 
