1f20aeba60495aefdc3f885febc69b2bc5770e82
mac <maciekklospl@gmail.com>
2023-01-29 18:01:59 +0100

    Section layout improved
    b: Section was mostly for automatically
    centering content.
    
    a: With classnames added option for modification.
    Fixed some small bugs from different browsers.

--- client/layouts/Section.tsx
+++ client/layouts/Section.tsx
@@ -1,16 +1,29 @@
+import classNames from 'classnames';
+
 type SectionProps = {
     children: React.ReactNode;
+    className?: string;
     maxWidth?: boolean;
     id?: string;
+    flex?: boolean;
 };
 
 const Section: React.FC<SectionProps> = ({
     children,
+    className,
     id,
+    flex,
     maxWidth = false,
 }) => (
     <section id={id} className="w-full py-28">
-        <div className={`${maxWidth ? 'max-w-full' : 'mx-auto max-w-7xl'}`}>
+        <div
+            className={classNames(
+                'md:px-12 px-6',
+                maxWidth ? 'max-w-full' : 'mx-auto max-w-7xl',
+                flex && 'flex justify-center lg:gap-24 md:gap-16 gap-6',
+                className && className
+            )}
+        >
             {children}
         </div>
     </section>
