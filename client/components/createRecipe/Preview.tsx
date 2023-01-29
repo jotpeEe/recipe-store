c4a7989917df9037f8d1db3a8c7fcda4ed93d42d
mac <maciekklospl@gmail.com>
2023-01-29 18:33:34 +0100

    created Preview component for createRecipe
    Feature which improves user experience during
    creation process.

---
+++ client/components/createRecipe/Preview.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+import Recipe from '@components/recipe';
+import { useAppSelector } from '@hooks';
+
+const Preview: FC = () => {
+    const { recipe, auth } = useAppSelector(state => state);
+
+    const { user } = auth;
+
+    return <Recipe {...{ ...recipe, user }} />;
+};
+
+export default Preview;
