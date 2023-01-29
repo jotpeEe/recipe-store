747be83baed799ae60cbe085168e11f64c985b6e
mac <maciekklospl@gmail.com>
2023-01-29 17:34:55 +0100

    updated Recipe and Review card components

--- client/components/card/Recipe.tsx
+++ client/components/card/Recipe.tsx
@@ -3,21 +3,21 @@ import Image from 'next/image';
 import { IconBookmark, IconStar } from '@icons';
 import { IRecipe } from '@lib/types';
 
-import { Card, Title, Link } from './CardWithLinks';
+import { Card, Title, Link } from '../CardWithLinks';
 
 const RecipeCard: React.FC<{ recipe?: Partial<IRecipe> }> = ({ recipe }) => {
-    const { image, title, prep } = recipe ?? {};
+    const { image, title, prep, id } = recipe ?? {};
     return (
-        <Card className="flex flex-col justify-center text-center items-center h-230 rounded-lg ">
+        <Card className="flex flex-col justify-center text-center items-center rounded-lg ">
             <div className="absolute bottom-0 left-0 bg-gray-100 w-full h-3/4 rounded-lg"></div>
 
-            <div className="relative">
+            <div className="relative overflow-hidden w-[100px] h-[100px] rounded-full">
                 <Image
                     width={110}
                     height={110}
                     src={image || '/default.png'}
                     alt="recipe image"
-                    className="z-10 rounded-full "
+                    className="z-10 inline h-full w-auto m-[0 auto]"
                 />
 
                 <div className="absolute flex items-center justify-center text-xs leading-normal py-1 px-2 rounded-3xl bg-yellow-100 -right-6 top-1/2 -translate-y-3/4 ">
@@ -26,21 +26,21 @@ const RecipeCard: React.FC<{ recipe?: Partial<IRecipe> }> = ({ recipe }) => {
                 </div>
             </div>
 
-            <h5 className="z-10 font-sans2 py-4 w-9/12 flex justify-center items-center ">
-                <Title className="font-sans" href="https://mklos.co">
+            <h5 className="z-10 font-sans2 py-4 break-words flex justify-center items-center ">
+                <Title className="font-sans" href={`/recipes/${id}`}>
                     {title}
                 </Title>
             </h5>
 
-            <div className="z-10 flex justify-between items-center w-full">
-                <div className="flex flex-col items-start p-3">
+            <div className="flex justify-between items-center w-full">
+                <div className="z-[5] flex flex-col items-start p-3">
                     <div className="text-xs pb-1">Time</div>
                     <div className="text-xs font-semibold">{prep} mins</div>
                 </div>
 
                 <Link
                     href="https://v1.mklos.co"
-                    className="flex justify-center items-center p-1 h-fit rounded-full bg-white mx-3"
+                    className="z-50 flex justify-center items-center p-1 h-fit rounded-full bg-white mx-3"
                 >
                     <IconBookmark />
                 </Link>
---
+++ client/components/card/Review.tsx
@@ -0,0 +1,64 @@
+import { useMemo } from 'react';
+
+import format from 'date-fns/format';
+
+import { IconNext } from '@icons';
+
+import { Card, Title, Link } from '../CardWithLinks';
+import RecipeTitle from '../recipe/Title';
+import UserInfo from '../user/Info';
+
+type ReviewProps = {
+    text: string;
+    recipe: {
+        id: string;
+        title: string;
+        image: string;
+    };
+    user: {
+        name: string;
+        photo: string;
+    };
+    createdAt: string;
+};
+
+const ReviewCard: React.FC<ReviewProps> = ({
+    text,
+    recipe,
+    user,
+    createdAt,
+}) => {
+    const { name, photo } = user ?? {};
+    const { id, title, image } = recipe ?? {};
+
+    const formattedDate = useMemo(() => {
+        const date = new Date(createdAt);
+        return format(date, 'MMMM dd, yyyy - HH:mm').toString();
+    }, [createdAt]);
+
+    return (
+        <Card className="flex">
+            <div className="flex flex-col p-8 max-w-md text-center shadow-card rounded-xl [&>*]:py-2">
+                <Title href={`/recipes/${id}`}>
+                    <RecipeTitle imgSrc={image} title={title} />
+                </Title>
+                <p className="text-lg text-left">{text}</p>
+                <UserInfo
+                    className="my-4"
+                    title={name}
+                    imgSrc={photo}
+                    subtitle={formattedDate}
+                />
+                <Link
+                    href={`/recipes/${id}`}
+                    className="flex justify-start gap-4 items-center w-fit "
+                >
+                    Go to recipe
+                    <IconNext />
+                </Link>
+            </div>
+        </Card>
+    );
+};
+
+export default ReviewCard;
