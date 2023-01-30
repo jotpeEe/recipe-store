3fcb6436fff8dd3f5281eac9262535dc98bc5a14
mac <maciekklospl@gmail.com>
2023-01-30 07:19:50 +0100

    finished feature reviews
    Implemented functionalities:
    - review cards
    - review CRUD backend
    - review listing
    - disable same user reviews
    
    to do:
    - add update/delete on frontend

--- client/components/ReviewList.tsx
+++ client/components/ReviewList.tsx
@@ -1,20 +1,64 @@
 import { FC, MouseEventHandler, useCallback, useState } from 'react';
 
+import { hasCookie } from 'cookies-next';
+import { useRouter } from 'next/router';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import { useQueryClient } from 'react-query';
+
+import { useCreateReviewMutation } from '@generated/graphql';
+import { useAppDispatch, useAppSelector } from '@hooks';
 import { IReview } from '@lib/types';
+import { requestClient } from '@requests';
 
 import Button from './Button';
 import ReviewMini from './card/ReviewMini';
+import FormInput from './input/Form';
 
 type ReviewListProps = {
     reviews?: IReview[];
+    id?: string;
+    addEnable?: boolean;
+    recipeAuthor?: {
+        id: string;
+        name: string;
+        photo: string;
+    };
 };
 
-const ReviewList: FC<ReviewListProps> = ({ reviews }) => {
+type ReviewInput = {
+    input: string;
+};
+
+const ReviewList: FC<ReviewListProps> = ({
+    reviews,
+    id,
+    addEnable,
+    recipeAuthor,
+}) => {
     const [clicked, setClicked] = useState(false);
+    const [open, setOpen] = useState(false);
+    const { user } = useAppSelector(state => state.auth);
+    const router = useRouter();
+    const queryClient = useQueryClient();
+
+    const isTheSameUser = recipeAuthor?.id === user?.id;
+
+    const methods = useForm<ReviewInput>();
+    const { handleSubmit, reset } = methods;
 
     const REVIEWS_LIMIT = 4;
     const [limit, setLimit] = useState(REVIEWS_LIMIT);
     const slicedReviews = reviews?.slice(0, limit);
+
+    const { mutate: createReview } = useCreateReviewMutation(requestClient, {
+        onSuccess() {
+            queryClient.refetchQueries('getReviewsByRecipe');
+            reset();
+            setOpen(state => !state);
+        },
+    });
+
+    const loggedIn = hasCookie('logged_in');
 
     const handleClick: MouseEventHandler<HTMLButtonElement> = useCallback(
         e => {
@@ -25,17 +69,71 @@ const ReviewList: FC<ReviewListProps> = ({ reviews }) => {
         [clicked]
     );
 
+    const handleCommentClick: MouseEventHandler<HTMLButtonElement> =
+        useCallback(
+            async e => {
+                e.preventDefault();
+                if (!loggedIn) router.push('/auth');
+                if (!user && loggedIn)
+                    await queryClient
+                        .refetchQueries('GetMe')
+                        .then(() => setOpen(state => !state));
+                if (user) setOpen(state => !state);
+            },
+            [user, loggedIn]
+        );
+
+    const onSubmit: SubmitHandler<ReviewInput> = useCallback(values => {
+        const { input } = values;
+        if (id) createReview({ input, id });
+    }, []);
+
     return (
-        <div className="max-h-[370px] overflow-y-auto">
-            {slicedReviews?.map((review, index) => (
-                <ReviewMini key={index} review={review} />
-            ))}
+        <div className="">
             {reviews && reviews?.length > REVIEWS_LIMIT && (
-                <div>
-                    <Button outlined onClick={handleClick} size="sm">
-                        {clicked ? 'Load less' : 'Load more'}
-                    </Button>
-                </div>
+                <button
+                    onClick={handleClick}
+                    className="flex items-center gap-3 bg-primary rounded-3xl w-full mb-4 shadow-card shadow-gray-400"
+                >
+                    <div className="rounded-full bg-white px-2 text-gray-500 font-medium ml-1 my-1">
+                        {clicked ? '-' : '+'}
+                    </div>
+                    <h5 className="text-xs text-white">
+                        {clicked ? 'Show less' : 'Show more'}
+                    </h5>
+                </button>
+            )}
+            <div className="max-h-[400px] overflow-y-auto">
+                {slicedReviews?.map((review, index) => (
+                    <ReviewMini key={index} review={review} />
+                ))}
+            </div>
+            {addEnable && !isTheSameUser && (
+                <>
+                    <button
+                        onClick={handleCommentClick}
+                        className="flex items-center gap-3 bg-primary rounded-3xl w-full mb-4 shadow-card shadow-gray-400"
+                    >
+                        <div className="rounded-full bg-white px-2 text-gray-500 font-medium ml-1 my-1">
+                            +
+                        </div>
+                        <h5 className="text-xs text-white">Add comment</h5>
+                    </button>
+                    {open && (
+                        <FormProvider {...methods}>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <FormInput
+                                    name="input"
+                                    placeholder="Enter comment"
+                                    element="textarea"
+                                />
+                                <Button type="submit" size="sm">
+                                    Add
+                                </Button>
+                            </form>
+                        </FormProvider>
+                    )}
+                </>
             )}
         </div>
     );
