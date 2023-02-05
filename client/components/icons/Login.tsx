9f2dff3e5691e0dcac46d6dbbcfdbea9c1bb3766
mac <maciekklospl@gmail.com>
2023-02-05 23:40:26 +0100

    fix: correct icons for mobile menu

--- client/components/icons/Login.tsx
+++ client/components/icons/Login.tsx
@@ -5,7 +5,7 @@ const Login = () => (
         xmlns="http://www.w3.org/2000/svg"
         fill="currentColor"
     >
-        <path d="m17 8-1.41 1.41L17.17 11H9v2h8.17l-1.58 1.58L17 16l4-4-4-4zM5 5h7V3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h7v-2H5V5z" />
+        <path d="M11 7 9.6 8.4l2.6 2.6H2v2h10.2l-2.6 2.6L11 17l5-5-5-5zm9 12h-8v2h8c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-8v2h8v14z" />
     </svg>
 );
 
