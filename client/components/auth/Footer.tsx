c979182d1a2effba14c7653f768a795cb10342c5
mac <maciekklospl@gmail.com>
2023-02-06 14:30:26 +0100

    refactor: Footer component update
    b: Some information was based on type props
    
    a: information based on slider step, removed type prop.

--- client/components/auth/Footer.tsx
+++ client/components/auth/Footer.tsx
@@ -7,13 +7,13 @@ type FooterProps = {
     type: 'login' | 'signup';
 };
 
-const Footer: FC<FooterProps> = ({ type }) => {
-    const { next, previous } = useSlider();
+const Footer: FC<FooterProps> = () => {
+    const { step, next, previous } = useSlider();
 
     return (
         <>
             <div className="w-full flex justify-center items-center before:w-10 before:h-px before:mr-2 before:bg-gray-300 after:w-10 after:h-px after:ml-2 after:bg-gray-300 text-gray-300">
-                <h6>Or {type === 'login' ? 'log in' : 'sign up'} with</h6>
+                <h6>Or {step === 0 ? 'log in' : 'sign up'} with</h6>
             </div>
             <div className="w-full flex gap-7 justify-center">
                 <a href="" className="p-2.5 rounded-xl border">
@@ -25,17 +25,15 @@ const Footer: FC<FooterProps> = ({ type }) => {
             </div>
             <div className="w-full flex justify-center items-center">
                 <h6>
-                    {type === 'login'
-                        ? `Don't have account? `
-                        : `Already a member? `}
+                    {step === 0 ? `Don't have account? ` : `Already a member? `}
                 </h6>{' '}
                 <button
-                    onClick={type === 'login' ? next : previous}
+                    onClick={step === 0 ? next : previous}
                     className="px-2 py-1"
                 >
                     <h6 className="text-amber-500">
                         {' '}
-                        {type === 'login' ? 'Sign up' : 'Log in'}
+                        {step === 0 ? 'Sign up' : 'Log in'}
                     </h6>
                 </button>
             </div>
