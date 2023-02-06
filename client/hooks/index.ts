c71eb21f1e087c0379a081834a6cf9bf62aa553f
mac <maciekklospl@gmail.com>
2023-02-06 14:39:17 +0100

    feat: preview hidden on createRecipe mobile.

--- client/hooks/index.ts
+++ client/hooks/index.ts
@@ -2,4 +2,5 @@ export { default as useLogout } from './useLogout';
 export { default as useRecipeQueries } from './useRecipeQueries';
 export { default as useRecipesFilter } from './useRecipesFilter';
 export { default as useScrollUp } from './useScrollUp';
+export { default as useWindowSize } from './useWindowSize';
 export { useAppDispatch, useAppSelector } from './redux';
---
+++ client/hooks/useWindowSize.ts
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+
+const useWindowSize = () => {
+    const [windowSize, setWindowSize] = useState<number | undefined>();
+    useEffect(() => {
+        const handleResize = () => setWindowSize(window.innerWidth);
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+    return windowSize;
+};
+
+export default useWindowSize;
