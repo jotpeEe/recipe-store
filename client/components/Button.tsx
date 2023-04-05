c96b24c6d9a10c0e98f787240510be37d948bcb8
mac <maciekklospl@gmail.com>
2023-04-05 16:29:04 +0200

    refactor: add variants of Button component

--- client/components/Button.tsx
+++ client/components/Button.tsx
@@ -17,7 +17,8 @@ type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
     isLoading?: boolean;
     outlined?: boolean;
     rotate?: boolean;
-    size?: 'xs' | 'sm' | 'md' | 'lg';
+    variant?: 'outlined' | 'pure' | 'normal' | 'alert' | 'input';
+    size?: 'xxs' | 'xs' | 'sm' | 'md' | 'lg';
 };
 
 const Button: FC<ButtonProps> = ({
@@ -26,6 +27,7 @@ const Button: FC<ButtonProps> = ({
     children,
     circle,
     className,
+    disabled,
     fullWidth,
     icon,
     isLoading,
@@ -34,24 +36,31 @@ const Button: FC<ButtonProps> = ({
     size = 'lg',
     type,
     hidden,
+    variant = 'normal',
     ...props
 }) => {
     const styles = classNames(
-        'flex items-center justify-center bg-primary gap-2 text-white font-semibold text-center transition-all h-fit',
-        circle
-            ? 'rounded-full p-4 text-black border border-black bg-white '
-            : 'rounded-xl',
-        size === 'xs' && !circle && 'text-xs py-0.5 px-3',
-        size === 'sm' && !circle && 'text-xs py-2 px-5 leading-none',
-        size === 'sm' && outlined && !circle && 'font-normal',
-        size === 'md' && !circle && 'px-5 py-2',
+        'flex items-center justify-center font-semibold gap-2 text-center transition-all h-fit border',
+        circle ? 'rounded-full p-4' : 'rounded-xl',
+        size === 'xxs' && 'text-xs p-0.5',
+        size === 'xs' && 'text-xs py-0.5 px-3',
+        size === 'sm' && 'text-xs py-2.5 px-5 leading-none',
+        size === 'md' && 'px-5 py-2',
         size === 'lg' && !circle && 'px-12 py-4',
+        variant === 'pure' &&
+            'bg-white text-secondary border-white hover:bg-gray-100',
+        variant === 'normal' &&
+            !disabled &&
+            'bg-primary text-white border-primary hover:bg-green-600',
+        variant === 'outlined' &&
+            'bg-white text-outlined border-outlined hover:bg-gray-100 hover:text-black',
+        variant === 'alert' &&
+            'bg-red-300 text-outlined border-outlined hover:bg-red-600 hover:text-black',
+        variant === 'input' &&
+            'bg-white text-outlined border-gray-200 hover:bg-gray-100 hover:text-black',
+        disabled && 'bg-gray-200 text-gray-500',
         fullWidth ? 'w-full' : 'w-fit',
-        outlined && !circle
-            ? 'bg-white text-outlined border'
-            : 'bg-primary text-white hover:bg-green-600',
         rotate && 'rotate-180',
-        (outlined || circle) && 'hover:bg-gray-200',
         hidden && 'opacity-0',
         className && `${className}`
     );
@@ -59,7 +68,12 @@ const Button: FC<ButtonProps> = ({
     return (
         <>
             {!href ? (
-                <button type={type} className={styles} {...props}>
+                <button
+                    type={type}
+                    className={styles}
+                    disabled={disabled}
+                    {...props}
+                >
                     {!isLoading ? (
                         <>
                             {icon && icon}
