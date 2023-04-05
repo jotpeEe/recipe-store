673a326169d65c16101cb54bbf6b62ba55ba93cb
mac <maciekklospl@gmail.com>
2023-04-05 18:49:59 +0200

    add index to components input

---
+++ client/components/input/index.ts
@@ -0,0 +1,8 @@
+export { default as Checkbox } from './Checkbox';
+export { default as ErrorMessage } from './ErrorMessage';
+export { default as IngredientInput } from './Ingredient';
+export { default as Input } from './Input';
+export { default as ImageInput } from './Image';
+export { default as Select } from './Select';
+export { default as Search } from './Search';
+export { default as TextArea } from './TextArea';
