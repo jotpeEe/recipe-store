54a2f1d440bc12608271b15933d1f606f08ca492
mac <maciekklospl@gmail.com>
2023-04-05 23:22:16 +0200

    feat: add zod image custom validation

---
+++ client/lib/imageValidation.ts
@@ -0,0 +1,23 @@
+import { type RefinementCtx } from 'zod';
+
+import { ACCEPTED_IMAGE_TYPES, MAX_IMAGE_SIZE } from '@constants';
+
+/**
+ * Callback for zod image validation, tests file type (.jpg, .jpeg, .png, .webp) and it's size.
+ */
+const imageValidation = (f: FileList | undefined, ctx: RefinementCtx) => {
+    if (!ACCEPTED_IMAGE_TYPES.includes(f?.[0]?.type)) {
+        ctx.addIssue({
+            code: 'custom',
+            message: 'Only .jpg, .jpeg, .png and .webp formats are supported',
+        });
+    }
+    if (f && f?.[0]?.size >= MAX_IMAGE_SIZE) {
+        ctx.addIssue({
+            code: 'custom',
+            message: 'Max image size is 500kb',
+        });
+    }
+};
+
+export default imageValidation;
