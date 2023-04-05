c1e0dacf85138c3cd0cec2e2ec528ded9cd7a94f
mac <maciekklospl@gmail.com>
2023-04-05 18:44:05 +0200

    feat: add textarea input

---
+++ client/components/input/TextArea.tsx
@@ -0,0 +1,54 @@
+import type { DetailedHTMLProps, FC, TextareaHTMLAttributes } from 'react';
+
+import cn from 'classnames';
+import { useFormContext } from 'react-hook-form';
+
+import ErrorMessage from './ErrorMessage';
+
+type TextAreaProps = DetailedHTMLProps<
+    TextareaHTMLAttributes<HTMLTextAreaElement>,
+    HTMLTextAreaElement
+> & {
+    name: string;
+    label?: string;
+    noValidation?: boolean;
+    value?: string | number;
+};
+
+const TextArea: FC<TextAreaProps> = ({
+    label,
+    name,
+    value,
+    noValidation,
+    ...props
+}) => {
+    const {
+        register,
+        formState: { errors },
+    } = useFormContext();
+
+    return (
+        <>
+            <div className="flex w-full flex-col items-start justify-center text-sm">
+                {label && (
+                    <label htmlFor={name} className={'mb-2 text-sm'}>
+                        {label}
+                    </label>
+                )}
+                <textarea
+                    defaultValue={value}
+                    rows={4}
+                    className={cn(
+                        'w-full resize-none rounded-lg border p-3 text-xs',
+                        errors[name] && 'border-red-600'
+                    )}
+                    {...props}
+                    {...register(name)}
+                />
+            </div>
+            {!noValidation && <ErrorMessage error={errors[name]} />}
+        </>
+    );
+};
+
+export default TextArea;
