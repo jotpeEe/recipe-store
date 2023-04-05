e6307576b1d9936eba8d2a3de5a376c830282c7b
mac <maciekklospl@gmail.com>
2023-04-05 17:32:27 +0200

    add svg icons

--- client/components/icons/Clock.tsx
+++ client/components/icons/Clock.tsx
@@ -1,7 +1,7 @@
 const Clock = () => (
     <svg
-        width="18"
-        height="18"
+        width="16"
+        height="16"
         viewBox="0 0 18 18"
         fill="currentColor"
         xmlns="http://www.w3.org/2000/svg"
