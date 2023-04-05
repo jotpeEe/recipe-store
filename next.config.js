fb1ff4ec627316d7a0bc639c103e968623b1d622
mac <maciekklospl@gmail.com>
2023-04-05 17:09:54 +0200

    configure i18n and eslint

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-param-reassign */
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions, import/extensions
+!process.env.SKIP_ENV_VALIDATION && (await import('./utils/env.mjs'));
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
@@ -16,7 +18,21 @@ const nextConfig = {
             },
         ],
     },
+    eslint: {
+        dirs: ['client', 'pages', 'server', 'styles', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+    },
+
+    /**
+     * If you have the "experimental: { appDir: true }" setting enabled, then you
+     * must comment the below `i18n` config out.
+     *
+     * @see https://github.com/vercel/next.js/issues/41980
+     */
+    i18n: {
+        locales: ['en'],
+        defaultLocale: 'en',
+    },
     output: 'standalone',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
