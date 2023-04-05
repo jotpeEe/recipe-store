dfc61d892c6df0b7aa7197211498204c8a845278
mac <maciekklospl@gmail.com>
2023-04-06 01:41:15 +0200

    feat-imp: update StepTwo and StepThree accordingly

---
+++ client/components/createRecipe/index.ts
@@ -0,0 +1,5 @@
+export { default as Panel } from './Panel';
+export { default as Preview } from './Preview';
+export { default as StepOne } from './StepOne';
+export { default as StepTwo } from './StepTwo';
+export { default as StepThree } from './StepThree';
---
+++ client/components/user/index.ts
@@ -0,0 +1,2 @@
+export { default as UserInfo } from './Info';
+export { default as UserStats } from './Stats';
