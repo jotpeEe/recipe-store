2bd85c5a9c8592774b8d214f9e09405732fff9b1
Maciej Klos <maciekklospl@gmail.com>
2023-04-27 09:56:13 +0200

    extract features from components

--- client/sections/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Hero } from './Hero';
-export { default as Login } from './Login';
-export { default as Recipes } from './Recipes';
-export { default as Reviews } from './Reviews';
-export { default as Signup } from './Signup';
