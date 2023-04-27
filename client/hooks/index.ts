c06bd869dede237ba17009be675dddd318fce760
Maciej Klos <maciekklospl@gmail.com>
2023-04-27 10:03:41 +0200

    delete hooks with condense queries

--- client/hooks/index.ts
+++ client/hooks/index.ts
@@ -1,6 +1,5 @@
 export { default as useKeyPress } from './useKeyPress';
 export { default as useLogout } from './useLogout';
-export { default as useRecipeQueries } from './useRecipeQueries';
 export { default as useRecipesFilter } from './useRecipesFilter';
 export { default as useScrollUp } from './useScrollUp';
 export { default as useWindowSize } from './useWindowSize';
