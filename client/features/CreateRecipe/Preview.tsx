e6a094c98c6e7dd695e4fe6331b68e1617b5eea5
Maciej Klos <maciekklospl@gmail.com>
2023-04-28 07:54:12 +0200

    refactor Preview component
    before: recipe preview state managed
    by redux.
    
    after: recipe straight from useForm watch
    subscription, saving state on server, updated
    by useForm defaultValues.

--- client/features/CreateRecipe/Preview.tsx
+++ client/features/CreateRecipe/Preview.tsx
@@ -1,14 +1,34 @@
-import { type FC } from 'react';
+import { type FC, useMemo } from 'react';
+
+import { useFormContext } from 'react-hook-form';
 
 import Recipe from '@components/recipe';
 import { useAppSelector } from '@hooks';
 
-const Preview: FC<{ withEdit?: boolean }> = ({ withEdit }) => {
-    const { recipe, auth } = useAppSelector(state => state);
+const Preview: FC = () => {
+    const user = useAppSelector(state => state.auth.user);
 
-    const { user } = auth;
+    const { watch, getValues } = useFormContext();
+    const formValues = getValues();
 
-    return <Recipe {...{ ...recipe, user, withEdit }} />;
+    const title = watch('title');
+    const description = watch('description');
+    const prep = watch('prep');
+    const image = watch('image');
+
+    const recipe = useMemo(
+        () => ({
+            ...formValues,
+            user,
+            title,
+            description,
+            prep,
+            image,
+        }),
+        [user, formValues, title, description, prep, image]
+    );
+
+    return <Recipe hideMobile {...recipe} />;
 };
 
 export default Preview;
