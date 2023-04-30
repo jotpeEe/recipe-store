a37ab7efd8db04d4dda6fa77435cb5d3e8c3e399
Maciej Klos <maciekklospl@gmail.com>
2023-04-30 23:46:22 +0200

    update types for rating feature

--- client/lib/types.ts
+++ client/lib/types.ts
@@ -1,7 +1,11 @@
-import { type GetMeQuery, type GetRecipeByIdQuery } from '@generated/graphql';
+import {
+    type GetMeQuery,
+    type GetProfileDataQueryQuery,
+    type GetRecipeByIdQuery,
+} from '@generated/graphql';
 
 export type IUser = GetMeQuery['getMe']['user'];
 
 export type IRecipe = GetRecipeByIdQuery['getRecipeById']['recipe'];
 
-export type IReview = GetRecipeByIdQuery['getReviewsBy']['reviews'][0];
+export type IReview = GetProfileDataQueryQuery['getMyReviews']['reviews'][0];
