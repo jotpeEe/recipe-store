04fa4ad7d9f1bc03df5060c7a4dcba822b598eee
Maciej Klos <maciekklospl@gmail.com>
2023-05-09 18:31:12 +0200

    feat: add zoom-in zoom-out on click to RecipeSteps.

--- client/features/RecipeView/Steps.tsx
+++ client/features/RecipeView/Steps.tsx
@@ -1,34 +1,55 @@
-import React, { type FC } from 'react';
+import React, { type FC, type MouseEvent, useState } from 'react';
+
+import cn from 'classnames';
 
 import { AnimateOnLoad } from '@components';
 import { useRecipeContext } from '@contexts';
 
 const Steps: FC = () => {
+    const [zoomed, setZoomed] = useState<number | undefined>();
     const { active, steps } = useRecipeContext();
+
+    const toggleZoom = (e: MouseEvent<HTMLDivElement>, index: number) => {
+        e.preventDefault();
+        if (zoomed === index) {
+            setZoomed(undefined);
+        } else {
+            setZoomed(index);
+        }
+    };
+
     return (
         <>
-            {steps &&
-                steps?.length !== 0 &&
-                active === 1 &&
-                steps.map(({ label, text }, index) => (
-                    <AnimateOnLoad key={index} index={index}>
-                        <div className="rounded-xl border-gray-200 shadow-card">
-                            {label && (
-                                <h6 className="rounded-xl px-3 py-4 text-xs ">
-                                    {label}
-                                </h6>
-                            )}
-                            <div className="h-[1px] w-full bg-gray-100"></div>
-                            <p
-                                className="break-words max-w-[36ch] px-3 py-4 text-xs text-gray-700 opacity-70"
-                                key={index}
-                                lang="pl"
+            {steps && steps?.length !== 0 && active === 'Steps' && (
+                <ul role="list" className="flex flex-col gap-3">
+                    {steps.map(({ label, text }, index) => (
+                        <AnimateOnLoad key={index} index={index} as="li">
+                            <div
+                                className={cn(
+                                    'rounded-xl  bg-white shadow-lg transition',
+                                    zoomed === index
+                                        ? 'z-[1000] scale-125 cursor-zoom-out'
+                                        : 'cursor-zoom-in'
+                                )}
+                                onClick={e => toggleZoom(e, index)}
                             >
-                                {text}
-                            </p>
-                        </div>
-                    </AnimateOnLoad>
-                ))}
+                                {label && (
+                                    <h6 className="break-words rounded-xl px-3 py-4 text-xs ">
+                                        {label}
+                                    </h6>
+                                )}
+                                <div className="h-[1px] w-full bg-gray-100"></div>
+                                <p
+                                    className="max-w-[42ch] break-words px-3 py-4 text-xs text-gray-700 opacity-70"
+                                    lang="pl"
+                                >
+                                    {text}
+                                </p>
+                            </div>
+                        </AnimateOnLoad>
+                    ))}
+                </ul>
+            )}
         </>
     );
 };
